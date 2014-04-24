d68bd8a33ab304d221f205e7865f73bb97b539ab
Sam Osterkil <pisketch@gmail.com>
2014-04-24 00:16:29 -0400

    Cleaned up the Blowfish reference

--- blowfish-reference.c
+++ blowfish-reference.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
 // C++ implementation of Blowfish, taken from Wikipedia, for reference.
 uint32_t P[18];     // P-array
 uint32_t S[4][256]; // S-boxes
@@ -6,7 +11,7 @@ uint32_t f (uint32_t x) {
 	uint32_t h = S[0][x >> 24] + S[1][x >> 16 & 0xff];
 	return ( h ^ S[2][x >> 8 & 0xff] ) + S[3][x & 0xff];
 }
-void encrypt (uint32_t & L, uint32_t & R) {
+void encrypt (uint32_t L, uint32_t R) {
 	for (int i=0 ; i<16 ; i += 2) {
 		L ^= P[i];
 		R ^= f(L);
@@ -17,7 +22,7 @@ void encrypt (uint32_t & L, uint32_t & R) {
 	R ^= P[17];
 	swap (L, R);
 }
-void decrypt (uint32_t & L, uint32_t & R) {
+void decrypt (uint32_t L, uint32_t R) {
 	for (int i=16 ; i > 0 ; i -= 2) {
 		L ^= P[i+1];
 		R ^= f(L);
@@ -32,7 +37,7 @@ void key_schedule (uint32_t key[], int keylen) {
 	// Initialize the P-Array and S-Boxes
 	for (int i=0 ; i<18 ; ++i)
 		P[i] ^= key[i % keylen];
-	uint32_t = 0, R = 0;
+	uint32_t L = 0, R = 0;
 	for (int i=0 ; i<18 ; i+=2) {
 		encrypt (L, R);
 		P[i] = L; P[i+1] = R;
