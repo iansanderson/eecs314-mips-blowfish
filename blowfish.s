d6578addbbb8e4cc088b4cfd8aef90bc2f3ec95b
Ian Anderson <isa12@case.edu>
2014-04-30 01:35:38 -0400

    Current state of affairs: thing still doesn't encrypt right. Well, we don't know that. We do know it doesn't decrypt right.

--- blowfish.s
+++ blowfish.s
@@ -1,21 +1,16 @@
 main:
-	la $a0, behaviorprompt	#load our behavior prompt into a0
-	li $v0, 4				#set v0 to 4 for string printing
-	syscall					#print our prompt
-	li $v0, 5				#set v0 to 5 for integer reading
-	syscall					#read input into v0
-	jal testinput			#test our input
-	add $s0, $zero, $v0		#copy v0 into s0 to save our behavior choice
+	li $s0, 1				#initialize behavior to 1(encrypt)
 	la $a0, inputprompt		#load our prompt for input here
 	li $v0, 4				#set v0 to 4 for string printing
 	syscall
 	la $a0, inputbuff		#load our input buffer's address into a0
+	li $a1, 4096			#load the size of the buffer into a1
 	li $v0, 8				#set v0 to 8 for string reading
 	syscall
 	la $a0, pockey			#load the key's address into a0 for keyschedule
 	li $a1, 1				#load the key's length, 1, into a1 for keyschedule
 	jal keysched			#call keyschedule.
-	li $t2, 65536			#set t2 to the length of the buffer because for now we don't care about output size efficiency.
+	li $t2, 4096			#set t2 to the length of the buffer because for now we don't care about output size efficiency.
 	la $a0, inputbuff		#load the input buffer's location for dencrypt
 	add $a1, $zero, $t2		#load the length of the buffer's contents into a1
 	jal dencrypt
@@ -24,38 +19,39 @@ main:
 	syscall
 	la $a0, inputbuff		#load the now-encrypted text
 	syscall
+	li $s0, 2				#set behavior to decrypt
 	jal dencrypt
 	la $a0, doutputtext		#load the message about decrypted text
+	li $v0, 4
+	syscall
+	la $a0, inputbuff		#load the now-decrypted text
 	syscall
 	j finish				#We're done here.
 
 swapendian:					#takes a0 as swapthis
-	#Comments that better explain what I'm about to do can be found in dencrypt. Sorry.
-	add $t6, $zero, $a0		#saving for the syscall we're about to make
-	li $v0, 9				#for allocating heap space
-	li $a0, 20				#for storing 5 registers in the heap
-	syscall					#allocate dat spaaaace
-	add $a0, $zero, $t6		#putting it back in its place
-	add $t6, $zero, $v0		#for using the address of the storage we just made
-	sw $t0, ($t6)
-	sw $t1, 4($t6)
-	sw $t2, 8($t6)
-	sw $t3, 12($t6)
-	sw $a0, 16($t6)
+	#Comments that better explain what I'm about to do can be found in dencrypt. Sorry.to make
+	addi $sp, $sp, -20		#move the stack pointer down for heap stuff
+	sw $t0, ($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+	sw $a0, 16($sp)
 	#done storing
-	lb $t0, 19($t6)			#tmp[0] = swapthis[3]
-	lb $t1, 18($t6)			#tmp[1] = swapthis[2]
-	lb $t2, 17($t6)			#tmp[2] = swapthis[1]
-	lb $t3, 16($t6)			#tmp[3] = swapthis[0]
-	sb $t0, 16($t6)			#swapthis[0] = tmp[0]
-	sb $t1, 17($t6)			#swapthis[1] = tmp[1]
-	sb $t2, 18($t6)			#swapthis[2] = tmp[2]
-	sb $t3, 19($t6)			#swapthis[3] = tmp[3]
+	lb $t0, 19($sp)			#tmp[0] = swapthis[3]
+	lb $t1, 18($sp)			#tmp[1] = swapthis[2]
+	lb $t2, 17($sp)			#tmp[2] = swapthis[1]
+	lb $t3, 16($sp)			#tmp[3] = swapthis[0]
+	sb $t0, 16($sp)			#swapthis[0] = tmp[0]
+	sb $t1, 17($sp)			#swapthis[1] = tmp[1]
+	sb $t2, 18($sp)			#swapthis[2] = tmp[2]
+	sb $t3, 19($sp)			#swapthis[3] = tmp[3]
 	#begin restoring
-	lw $t0, ($t6)
-	lw $t1, 4($t6)
-	lw $t2, 8($t6)
-	lw $t3, 12($t6)
+	lw $t0, ($sp)
+	lw $t1, 4($sp)
+	lw $t2, 8($sp)
+	lw $t3, 12($sp)
+	lw $a0, 16($sp)
+	addi $sp, $sp, 20
 	jr $ra					#jump back to where we came from
 
 f:							#takes a0 as "x"
@@ -83,33 +79,28 @@ f:							#takes a0 as "x"
 	jr $ra					#jump back to where we came here from.
 
 dencrypt:					#takes a0 as both the source and destination(we put it where we got it), and takes a1 as the length in bytes of the data(after padding)
-	#For sake of ease, we're going to store the values of all t and s registers, other than s4, and ra in the heap. s4 will be used to remember where in the heap that storage is.
+	#For sake of ease, we're going to store the values of all t and s registers, other than s4, and ra in the heap.
 	#First though, it'll be used to backup a0 for allocating said memory.
 	#Yes, we understand what we're doing here. Scoping in assembly is the best feature.
-	add $s4, $zero, $a0		#aforementioned backup
-	li $v0, 9				#for syscalling allocation
-	li $a0, 92				#88 = 10*4 + 8*4 + 4 + 4*4 + 4= byte size of all t + (byte size of (all s - 1)) + bite size of ra + byte size of the 4 temps we want to save in the loop + byte size of a0
-	syscall					#do the allocation
-	add $a0, $zero, $s4		#put the old a0 back in there
-	add $s4, $zero, $v0		#get the location of the heap storage into s4
-	sw $t0, ($s4)
-	sw $t1, 4($s4)
-	sw $t2, 8($s4)
-	sw $t3, 12($s4)
-	sw $t4, 16($s4)
-	sw $t5, 20($s4)
-	sw $t6, 24($s4)
-	sw $t7, 28($s4)
-	sw $t8, 32($s4)
-	sw $t9, 36($s4)
-	sw $s0, 40($s4)
-	sw $s1, 44($s4)
-	sw $s2, 48($s4)
-	sw $s3, 52($s4)
-	sw $s5, 56($s4)
-	sw $s6, 60($s4)
-	sw $s7, 64($s4)
-	sw $ra, 68($s4)
+	addi $sp, $sp, -92				#92 bytes for all registers we plan to store
+	sw $t0, ($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+	sw $t4, 16($sp)
+	sw $t5, 20($sp)
+	sw $t6, 24($sp)
+	sw $t7, 28($sp)
+	sw $t8, 32($sp)
+	sw $t9, 36($sp)
+	sw $s0, 40($sp)
+	sw $s1, 44($sp)
+	sw $s2, 48($sp)
+	sw $s3, 52($sp)
+	sw $s5, 56($sp)
+	sw $s6, 60($sp)
+	sw $s7, 64($sp)
+	sw $ra, 68($sp)
 	#done storing
 	li $t0, 0				#loop variable
 	add $t1, $zero, $a1		#loop condition
@@ -122,21 +113,21 @@ denloop:
 		addi $t2, $t2, 4		#add 1 for accessing next item
 		lw $a3, ($t2)			#load the "i*2+1"th item
 		li $t3, 1				#for behavior checking
-		sw $t0, 72($s4)			#prevent data corruption
-		sw $t1, 76($s4)			#prevent data corruption
-		sw $t2, 80($s4)			#prevent data corruption
-		sw $t3, 84($s4)			#prevent data corruption
-		sw $a0, 88($s4)			#prevent moar data corruption
+		sw $t0, 72($sp)			#prevent data corruption
+		sw $t1, 76($sp)			#prevent data corruption
+		sw $t2, 80($sp)			#prevent data corruption
+		sw $t3, 84($sp)			#prevent data corruption
+		sw $a0, 88($sp)			#prevent moar data corruption
 		beq $t3, $s0, denlen	#go to where we encrypt if that's what we're supposed to do
 		jal decryptblock		#otherwise, decrypt
 		j denlr					#and skip the encryption because no
 denlen:	jal encryptblock
 denlr:
-		lw $t0, 72($s4)			#bring back values
-		lw $t1, 76($s4)			#bring back values
-		lw $t2, 80($s4)			#bring back values
-		lw $t3, 84($s4)			#bring back values
-		lw $a0, 88($s4)			#bring back moar values
+		lw $t0, 72($sp)			#bring back values
+		lw $t1, 76($sp)			#bring back values
+		lw $t2, 80($sp)			#bring back values
+		lw $t3, 84($sp)			#bring back values
+		lw $a0, 88($sp)			#bring back moar values
 		sw $a3, ($t2)			#store the en/decrypted a3 back in the "i*2+1"th place
 		addi $t2, $t2, -4		#subtract 1 for accessing the previous item
 		sw $a2, ($t2)			#store the en/decrypted a2 back in the "i*2"th place
@@ -144,24 +135,25 @@ denlr:
 		j denloop				#keep looping
 enddenloop:
 	#begin restoring
-	lw $t0, ($s4)
-	lw $t1, 4($s4)
-	lw $t2, 8($s4)
-	lw $t3, 12($s4)
-	lw $t4, 16($s4)
-	lw $t5, 20($s4)
-	lw $t6, 24($s4)
-	lw $t7, 28($s4)
-	lw $t8, 32($s4)
-	lw $t9, 36($s4)
-	lw $s0, 40($s4)
-	lw $s1, 44($s4)
-	lw $s2, 48($s4)
-	lw $s3, 52($s4)
-	lw $s5, 56($s4)
-	lw $s6, 60($s4)
-	lw $s7, 64($s4)
-	lw $ra, 68($s4)
+	lw $t0, ($sp)
+	lw $t1, 4($sp)
+	lw $t2, 8($sp)
+	lw $t3, 12($sp)
+	lw $t4, 16($sp)
+	lw $t5, 20($sp)
+	lw $t6, 24($sp)
+	lw $t7, 28($sp)
+	lw $t8, 32($sp)
+	lw $t9, 36($sp)
+	lw $s0, 40($sp)
+	lw $s1, 44($sp)
+	lw $s2, 48($sp)
+	lw $s3, 52($sp)
+	lw $s5, 56($sp)
+	lw $s6, 60($sp)
+	lw $s7, 64($sp)
+	lw $ra, 68($sp)
+	addi $sp, $sp, -92
 	jr $ra					#jump back to where we came from
 
 encryptblock:				#takes a2 as "L" and a3 as "R".
@@ -372,7 +364,7 @@ finish:
 .data
 #ifilebuff: .space 1600
 #ofilebuff: .space 1600
-inputbuff: .space 65536
+inputbuff: .space 4096
 buffer: .space 64
 plist: .word 0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344, 0xa4093822, 0x299f31d0, 0x082efa98, 0xec4e6c89, 0x452821e6, 0x38d01377, 0xbe5466cf, 0x34e90c6c, 0xc0ac29b7, 0xc97c50dd, 0x3f84d5b5, 0xb5470917, 0x9216d5d9, 0x8979fb1b
 slistone: .word 0xd1310ba6, 0x98dfb5ac, 0x2ffd72db, 0xd01adfb7, 0xb8e1afed, 0x6a267e96, 0xba7c9045, 0xf12c7f99, 0x24a19947, 0xb3916cf7, 0x0801f2e2, 0x858efc16, 0x636920d8, 0x71574e69, 0xa458fea3, 0xf4933d7e, 0x0d95748f, 0x728eb658, 0x718bcd58, 0x82154aee, 0x7b54a41d, 0xc25a59b5, 0x9c30d539, 0x2af26013, 0xc5d1b023, 0x286085f0, 0xca417918, 0xb8db38ef, 0x8e79dcb0, 0x603a180e, 0x6c9e0e8b, 0xb01e8a3e, 0xd71577c1, 0xbd314b27, 0x78af2fda, 0x55605c60, 0xe65525f3, 0xaa55ab94, 0x57489862, 0x63e81440, 0x55ca396a, 0x2aab10b6, 0xb4cc5c34, 0x1141e8ce, 0xa15486af, 0x7c72e993, 0xb3ee1411, 0x636fbc2a, 0x2ba9c55d, 0x741831f6, 0xce5c3e16, 0x9b87931e, 0xafd6ba33, 0x6c24cf5c, 0x7a325381, 0x28958677, 0x3b8f4898, 0x6b4bb9af, 0xc4bfe81b, 0x66282193, 0x61d809cc, 0xfb21a991, 0x487cac60, 0x5dec8032, 0xef845d5d, 0xe98575b1, 0xdc262302, 0xeb651b88, 0x23893e81, 0xd396acc5, 0x0f6d6ff3, 0x83f44239, 0x2e0b4482, 0xa4842004, 0x69c8f04a, 0x9e1f9b5e, 0x21c66842, 0xf6e96c9a, 0x670c9c61, 0xabd388f0, 0x6a51a0d2, 0xd8542f68, 0x960fa728, 0xab5133a3, 0x6eef0b6c, 0x137a3be4, 0xba3bf050, 0x7efb2a98, 0xa1f1651d, 0x39af0176, 0x66ca593e, 0x82430e88, 0x8cee8619, 0x456f9fb4, 0x7d84a5c3, 0x3b8b5ebe, 0xe06f75d8, 0x85c12073, 0x401a449f, 0x56c16aa6, 0x4ed3aa62, 0x363f7706, 0x1bfedf72, 0x429b023d, 0x37d0d724, 0xd00a1248, 0xdb0fead3, 0x49f1c09b, 0x075372c9, 0x80991b7b, 0x25d479d8, 0xf6e8def7, 0xe3fe501a, 0xb6794c3b, 0x976ce0bd, 0x04c006ba, 0xc1a94fb6, 0x409f60c4, 0x5e5c9ec2, 0x196a2463, 0x68fb6faf, 0x3e6c53b5, 0x1339b2eb, 0x3b52ec6f, 0x6dfc511f, 0x9b30952c, 0xcc814544, 0xaf5ebd09, 0xbee3d004, 0xde334afd, 0x660f2807, 0x192e4bb3, 0xc0cba857, 0x45c8740f, 0xd20b5f39, 0xb9d3fbdb, 0x5579c0bd, 0x1a60320a, 0xd6a100c6, 0x402c7279, 0x679f25fe, 0xfb1fa3cc, 0x8ea5e9f8, 0xdb3222f8, 0x3c7516df, 0xfd616b15, 0x2f501ec8, 0xad0552ab, 0x323db5fa, 0xfd238760, 0x53317b48, 0x3e00df82, 0x9e5c57bb, 0xca6f8ca0, 0x1a87562e, 0xdf1769db, 0xd542a8f6, 0x287effc3, 0xac6732c6, 0x8c4f5573, 0x695b27b0, 0xbbca58c8, 0xe1ffa35d, 0xb8f011a0, 0x10fa3d98, 0xfd2183b8, 0x4afcb56c, 0x2dd1d35b, 0x9a53e479, 0xb6f84565, 0xd28e49bc, 0x4bfb9790, 0xe1ddf2da, 0xa4cb7e33, 0x62fb1341, 0xcee4c6e8, 0xef20cada, 0x36774c01, 0xd07e9efe, 0x2bf11fb4, 0x95dbda4d, 0xae909198, 0xeaad8e71, 0x6b93d5a0, 0xd08ed1d0, 0xafc725e0, 0x8e3c5b2f, 0x8e7594b7, 0x8ff6e2fb, 0xf2122b64, 0x8888b812, 0x900df01c, 0x4fad5ea0, 0x688fc31c, 0xd1cff191, 0xb3a8c1ad, 0x2f2f2218, 0xbe0e1777, 0xea752dfe, 0x8b021fa1, 0xe5a0cc0f, 0xb56f74e8, 0x18acf3d6, 0xce89e299, 0xb4a84fe0, 0xfd13e0b7, 0x7cc43b81, 0xd2ada8d9, 0x165fa266, 0x80957705, 0x93cc7314, 0x211a1477, 0xe6ad2065, 0x77b5fa86, 0xc75442f5, 0xfb9d35cf, 0xebcdaf0c, 0x7b3e89a0, 0xd6411bd3, 0xae1e7e49, 0x00250e2d, 0x2071b35e, 0x226800bb, 0x57b8e0af, 0x2464369b, 0xf009b91e, 0x5563911d, 0x59dfa6aa, 0x78c14389, 0xd95a537f, 0x207d5ba2, 0x02e5b9c5, 0x83260376, 0x6295cfa9, 0x11c81968, 0x4e734a41, 0xb3472dca, 0x7b14a94a, 0x1b510052, 0x9a532915, 0xd60f573f, 0xbc9bc6e4, 0x2b60a476, 0x81e67400, 0x08ba6fb5, 0x571be91f, 0xf296ec6b, 0x2a0dd915, 0xb6636521, 0xe7b9f9b6, 0xff34052e, 0xc5855664, 0x53b02d5d, 0xa99f8fa1, 0x08ba4799, 0x6e85076a
